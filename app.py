adff574d5be7ddd7ba86fc14da80d2e662cb20ad
irfan-za <fanzahran123@gmail.com>
2023-09-16 23:22:45 +0700

    initial files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from pytube import YouTube
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["POST"])
+def download_video():
+    data = request.get_json()
+    video_url = data["videoURL"]
+    resolution = data["resolution"]
+    
+    try:
+        yt = YouTube(video_url)
+        if resolution == "audio":
+            stream = yt.streams.filter(only_audio=True).first()
+        else:
+            stream = yt.streams.filter(res=f"{resolution}p").first()
+        stream.download(output_path="C:/Users/irfan/Downloads")
+        return jsonify({"message": "Video berhasil diunduh!"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/app.js
@@ -0,0 +1,33 @@
+const downloadForm = document.getElementById('downloadForm');
+const downloadMessage = document.getElementById("downloadMessage");
+const downloadButton = document.getElementById("downloadButton");
+
+downloadForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  downloadButton.dosabled=true;
+  const videoURL = document.getElementById("videoURL").value;
+  const resolution = document.getElementById("resolution").value;
+  const response = await fetch("http://127.0.0.1:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ videoURL, resolution }),
+  })
+  .then(async(response)=>{
+    if(response.ok){
+    const data = await response.json();
+    downloadMessage.innerHTML = `<p class='text-green-500'>${data.message}</p>`;
+    }
+    else {
+      const data = await response.json();
+      downloadMessage.innerHTML = `<p class='text-red-500 font-medium'>Error :${data.error } <br> Silahkan coba Url berbeda atau coba beberapa saat lagi.</p>`;
+    }
+  })
+  .finally(()=>{
+    downloadButton.disabled=false
+  });
+
+
+});
+
